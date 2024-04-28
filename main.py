fcf7763db6c2be5be30140a0fa310efb9e8b70ee
Brunna <bgabriellams@gmail.com>
2024-04-28 17:36:53 -0300

    aaaaaaaaaaa

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
-    
-@app.route("/etapa1")
-def etapa1():
-    return render_template('etapa1.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
