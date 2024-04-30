53aa8309f2c2f9efd30ee6c8ce1847a2d6125724
Brunna <bgabriellams@gmail.com>
2024-04-29 21:59:45 -0300

    Removendo coisas randomicas
    Removida o anchor do html e removido a funcao de download mal feita

--- main.py
+++ main.py
@@ -6,33 +6,12 @@ from flask import Flask,url_for,render_template,send_file
 
 app = Flask(__name__)
 
-diretorio = os.path.abspath(os.path.dirname(__file__))
 
 @app.route("/")
 def home():
     titulo = "Catalogo"
     catalogo = database.lista_de_produtos()
     return render_template("index.html",titulo=titulo,catalogo=catalogo)
-
-@app.route('/download')
-def download():
-    path = 'database.json'
-
-    #Abre o arquivo JSON
-    with open(path, 'r') as file:
-        Arqui_JSON = json.load(file)
-
-
-    #Gera arquivo ZIP
-
-    zip_path = 'database.zip'
-
-    with zipfile.ZipFile(zip_path, 'w') as zipf:
-        zipf.write(path, 'database.json')
- 
-
-    return send_file(zip_path, as_attachment=True)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
