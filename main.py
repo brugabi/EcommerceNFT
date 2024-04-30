c93072345c8405a3595b94304967a33511046796
Brunna <bgabriellams@gmail.com>
2024-04-29 22:51:10 -0300

    kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import modules.database as database
-import zipfile
-import os
-import json
+
 from flask import Flask,url_for,render_template,send_file
 
 app = Flask(__name__)
@@ -17,9 +15,6 @@ def home():
 def return_file():
     return send_file('./arquivos/data_exported.zip', as_attachment=True)
 
-@app.route('/file-downloads/')
-def file_downloads():
-    return render_template('download.html')
 if __name__ == "__main__":
     app.run(debug=True)
 
