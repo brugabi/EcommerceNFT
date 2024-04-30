148d570e41b386ccd20555cae3ffd11fa392c412
Brunna <bgabriellams@gmail.com>
2024-04-29 22:42:34 -0300

    Download do zip file
    Feito um metodo para fazer download do zipfile

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ def home():
     catalogo = database.lista_de_produtos()
     return render_template("index.html",titulo=titulo,catalogo=catalogo)
 
+@app.route('/return-file')
+def return_file():
+    return send_file('./arquivos/data_exported.zip', as_attachment=True)
+
+@app.route('/file-downloads/')
+def file_downloads():
+    return render_template('download.html')
 if __name__ == "__main__":
     app.run(debug=True)
 
