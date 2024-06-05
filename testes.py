6d061e78592037f65c9668ce3961c9908ffb7a3e
brunna <bgabriellams@gmail.com>
2024-06-05 10:36:14 -0300

    NFts

--- testes.py
+++ testes.py
@@ -4,3 +4,4 @@ from modules import database
 # Testando a função
 resultados = database.ler_banco_de_dados_arvore_b(min_key=4, max_key=8, min_value=10000, substring="Test")
 print("Registros filtrados:", json.dumps(resultados,indent=2))
+
